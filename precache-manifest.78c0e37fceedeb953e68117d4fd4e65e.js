6c6f7013019b1f72513241ff96ffbcf1a1ee49f8
sunr <Suneetha.Rajanna@softwareag.com>
2020-07-24 21:59:57 +0530

    Updates

---
+++ precache-manifest.78c0e37fceedeb953e68117d4fd4e65e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a43024df1333449404f845781ef7a72d",
+    "url": "/suneethar/Portfolio1/index.html"
+  },
+  {
+    "revision": "c27a161833886e54e975",
+    "url": "/suneethar/Portfolio1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6240303912ba9da13740",
+    "url": "/suneethar/Portfolio1/static/js/2.0feb8754.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/suneethar/Portfolio1/static/js/2.0feb8754.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c27a161833886e54e975",
+    "url": "/suneethar/Portfolio1/static/js/main.111a0676.chunk.js"
+  },
+  {
+    "revision": "6c73db82542b4ee28a04",
+    "url": "/suneethar/Portfolio1/static/js/runtime-main.fb0f50af.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/suneethar/Portfolio1/static/media/logo.5d5d9eef.svg"
+  }
+]);
